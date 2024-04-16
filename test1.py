f3a6b47bed07076bc867e4aea8706769c0f71bbe
buitrekommando <jl.henriquez@gmail.com>
2024-04-16 16:46:22 -0400

    codigo clonado
    win y mac el mismo

--- test1.py
+++ test1.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv("datos.csv")
-df.head(3)
-print(df.info())
+data_frame = pd.read_csv("datos.csv")
+data_frame.head(3)
+
+#print(data_frame) #imprime solo los 5 primeros y 5 ultimos
+#print(frame_datos.to_string()) #imprime todo el Dataframe
+#print(pd.options.display.max_rows) 
+
+#Limpiar Data: 
+data_frame.dropna(inplace = True)
+print(data_frame.info())
